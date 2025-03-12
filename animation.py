5374586e1df63ae6206888395be4d7ba82f15e06
yarg niy <grayyin@outlook.com>
2025-03-12 23:43:23 +0800

    0.5 (不可用版本) 1、开发了transform.py,实现了视频数据的提取 2、优化了结构 注意：当前版本animation不可用！ 将改为csv读取

--- animation.py
+++ animation.py
@@ -1,10 +1,10 @@
-import pandas,time, curses
+import pandas, time, curses
 from itertools import islice
 
 def main(stdscr):
     curses.curs_set(0)  # 隐藏光标
-    stdscr.nodelay(1)   # 非阻塞输入
-    frame_rate = 10  # 帧率
+    stdscr.nodelay(1)  # 非阻塞输入
+    frame_rate = 24  # 帧率
     frame_delay = 1.0 / frame_rate  # 每帧的延迟时间
 
     index = -1
@@ -14,24 +14,28 @@ def main(stdscr):
         x, y = 0, 0
         stdscr.clear()
 
-        for cur_index, row in islice( data.iterrows(), index+1, None ):
+        for cur_index, row in islice(data.iterrows(), index + 1, None):
             #一行以内
             x = 0
             index = cur_index
-            if row[0]==-1:
+            if row[0] == -1:
                 print("over")
                 break
 
             for i in row:
                 i = int(i)
-                if i>100:
-                    for j in range(i%100):
-                        stdscr.addstr(y, x+j, "*")
+                if i > 400:
+                    for j in range(i % 200):
+                        stdscr.addstr(y, x + j, "*")
+                elif i > 200:
+                    for j in range(i % 200):
+                        stdscr.addstr(y, x + j, "-")
                 else:
-                    for j in range(i%100):
-                        stdscr.addstr(y, x+j, " ")
-                x += (i%100)
-            y+=1
+                    for j in range(i % 200):
+                        stdscr.addstr(y, x + j, " ")
+
+                x += (i % 200)
+            y += 1
         stdscr.refresh()
 
         # 动态控制延时，稳定刷新率
@@ -41,8 +45,9 @@ def main(stdscr):
 
 if __name__ == "__main__":
     try:
-        file_path = r"D:\py\Character-Animation\animation.csv"
-        data = pandas.read_csv(file_path, dtype=float, header=None)
+        file_path = r"D:\py\Character-Animation\media.csv"
+        data = pandas.read_csv(file_path, dtype=float, header=None, na_values=0)
+        data.fillna(0)
         curses.wrapper(main)
 
     except KeyboardInterrupt:
---
+++ transform.py
@@ -0,0 +1,54 @@
+import cv2
+import pandas as pd
+import numpy as np
+
+media_path = "media.mp4"
+capture = cv2.VideoCapture(media_path)
+
+file_path = r"media.csv"
+
+if capture.isOpened() is False:
+    print("Error opening video stream or file")
+
+frame_index = 0
+print("Starting in frame: '{}'".format(frame_index))
+
+total = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+print(total)
+while capture.isOpened() and frame_index < total:
+    capture.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+    ret, frame = capture.read()
+
+    if ret:
+        stretched_frame = cv2.resize(frame, (0, 0), fx=0.141, fy=0.0625 )  # 裁切
+        gray_frame = cv2.cvtColor(stretched_frame, cv2.COLOR_BGR2GRAY)    # 灰度
+
+        frame = np.array(gray_frame)
+        frame  = frame // 75
+        data =  list([0] for _ in range(30))
+        for i in range(30):
+            for j in frame[i]:
+                if j == 0:
+                    if data[i][-1] <200:
+                        data[i][-1] += 1
+                    else:
+                        data[i].append(1)
+                elif j == 1:
+                    if 200 < data[i][-1] <400:
+                        data[i][-1] += 1
+                    else:
+                        data[i].append(201)
+                else :
+                    if 200 < data[i][-1]:
+                        data[i][-1] += 1
+                    else:
+                        data[i].append(401)
+        data.append([-1])
+        df = pd.DataFrame(data)
+        df.to_csv(file_path, mode='a', index=False, header=False)
+        frame_index += 1
+
+        print("Complete: ",  frame_index, "/", total)
+
+    else:
+        break
