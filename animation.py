fcb0c3c46449fd61d736edf149681c5f748951a4
yarg niy <grayyin@outlook.com>
2025-03-12 15:22:35 +0800

    0.1 删除多余代码，优化结构

--- animation.py
+++ animation.py
@@ -1,27 +1,13 @@
-import time,os,pandas
+import os,pandas
 import curses
 
 def clear_screen():
     os.system('cls')
 
-def animate(data):
-    while True:
-        clear_screen()
-        for index, row in data.iterrows():
-            for i in row:
-                if i>100:
-                    print("*"*(i%100), end="")
-                else:
-                    print(" "*i, end="")
-            print("\n", end="")
-        time.sleep(0.04)
-
 def main(stdscr):
     curses.curs_set(0)  # 隐藏光标
     stdscr.nodelay(1)   # 非阻塞输入
     stdscr.timeout(100) # 每100毫秒刷新一次
-    file_path = r"D:\py\birthday0502\animation.csv"
-    data = pandas.read_csv(file_path, dtype=int, header=None)
 
     while True:
         #一帧以内
@@ -42,9 +28,10 @@ def main(stdscr):
             y+=1
         stdscr.refresh()
 
-
 if __name__ == "__main__":
     try:
+        file_path = r"D:\py\birthday0502\animation.csv"
+        data = pandas.read_csv(file_path, dtype=int, header=None)
         curses.wrapper(main)
 
     except KeyboardInterrupt:
