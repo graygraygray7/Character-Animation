6428161d0ede6db406810e4104bfe78eda459582
yarg niy <grayyin@outlook.com>
2025-03-15 13:08:50 +0800

    1.0 (正式版本！) 完成了animation的重构 优化了transform的写入方式 目前支持24帧视频的三灰度

--- animation.py
+++ animation.py
@@ -1,5 +1,5 @@
-import pandas, time, curses
-from itertools import islice
+import _curses
+import csv, time, curses
 
 def main(stdscr):
     curses.curs_set(0)  # 隐藏光标
@@ -7,47 +7,68 @@ def main(stdscr):
     frame_rate = 24  # 帧率
     frame_delay = 1.0 / frame_rate  # 每帧的延迟时间
 
-    index = -1
-    while True:
+    with open(file_path) as file:
+        data = csv.reader(file)
         # 一帧以内
         start_time = time.time()
+        global x
+        global y
         x, y = 0, 0
         stdscr.clear()
 
-        for cur_index, row in islice(data.iterrows(), index + 1, None):
+        for row in data:
+            row = ["0" if i == '' else i for i in row]
+            row = list(map(int, map(float, row)))
             #一行以内
-            x = 0
-            index = cur_index
             if row[0] == -1:
+                stdscr.refresh()
+
+                # 动态控制延时，稳定刷新率
+                elapsed_time = time.time() - start_time
+                if elapsed_time < frame_delay:
+                    time.sleep(frame_delay - elapsed_time)
+
+                start_time = time.time()
+                x, y = 0, 0
+                stdscr.clear()
+
                 print("over")
-                break
+                continue
 
             for i in row:
                 i = int(i)
                 if i > 400:
                     for j in range(i % 200):
-                        stdscr.addstr(y, x + j, "*")
+                        try:
+                            stdscr.addstr(y, x + j, '*')
+                        except _curses.error:
+                            pass
                 elif i > 200:
                     for j in range(i % 200):
-                        stdscr.addstr(y, x + j, "-")
+                        try:
+                            stdscr.addstr(y, x + j, '-')
+                        except _curses.error:
+                            pass
                 else:
                     for j in range(i % 200):
-                        stdscr.addstr(y, x + j, " ")
+                        try:
+                            stdscr.addstr(y, x + j, ' ')
+                        except _curses.error:
+                            # 功勋代码，舍不得删
+                            # print(y, x+j)
+                            # print(curses.LINES, curses.COLS)
+                            # time.sleep(0.5)
+                            # raise ValueError(y, x+j)
+                            # x = input("d")
+                            pass
 
                 x += (i % 200)
             y += 1
-        stdscr.refresh()
-
-        # 动态控制延时，稳定刷新率
-        elapsed_time = time.time() - start_time
-        if elapsed_time < frame_delay:
-            time.sleep(frame_delay - elapsed_time)
+            x = 0
 
 if __name__ == "__main__":
     try:
         file_path = r"D:\py\Character-Animation\media.csv"
-        data = pandas.read_csv(file_path, dtype=float, header=None, na_values=0)
-        data.fillna(0)
         curses.wrapper(main)
 
     except KeyboardInterrupt:
