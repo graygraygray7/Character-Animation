550f48b8d1d5661a6d6616dbea3d6ed01cb61e13
yarg niy <grayyin@outlook.com>
2025-03-25 15:35:57 +0800

    1.45 优化项目代码

--- trans_test.py
+++ trans_test.py
@@ -1,5 +1,7 @@
 import cv2, json
 import time
+import _curses
+
 import pandas as pd
 import numpy as np
 
@@ -9,46 +11,53 @@ with open(json_path) as file:
     csv_path = control["csv_path"]
     media_path = control["media_path"]
 
-capture = cv2.VideoCapture(media_path)
+cap = cv2.VideoCapture(media_path)
 
-if capture.isOpened() is False:
+if cap.isOpened() is False:
     print("Error opening video stream or file")
 
 frame_index = 0
 print("Starting in frame: '{}'".format(frame_index))
 
-total = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+total = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 print(total)
 
 fps = 24
 frame_interval = 1.0/fps
+width = float(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = float(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 prev_time = 0.0
+cha = False
 
-while capture.isOpened() and frame_index < total:
+print(width, height)
+while cap.isOpened() and frame_index < total:
     start_time = time.time()
     if start_time - prev_time >= frame_interval:
-        capture.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        ret, frame = capture.read()
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+        ret, frame = cap.read()
 
         if ret:
             div = 128
             # stretched_frame = cv2.resize(frame, (0, 0), fx = 0.141, fy=0.0625)  # 裁切
-            stretched_frame = cv2.resize(frame, (0, 0), fx=0.225, fy=0.1)  # 裁切
+            stretched_frame = cv2.resize(frame, (0, 0), fx=120.0/width, fy=30.0/height)  # 裁切
+            stretched_frame = cv2.resize(stretched_frame, (0, 0), fx=width/120, fy=height/30.0)  # 裁切
             gray_frame = cv2.cvtColor(stretched_frame, cv2.COLOR_BGR2GRAY)   # 灰度
             gray_frame = gray_frame//32 * 32
-            # frame = frame // div *div
+            # frame = gray_frame
+            frame = stretched_frame // div *div
 
-            cv2.imshow("railgun", gray_frame)
-            prev_time = start_time
+            if not cha:
+                cv2.imshow("railgun", frame)
+                prev_time = start_time
 
-            print("Complete: ",  frame_index, "/", total)
-            frame_index += 1
-            # x = input()
+                print("Complete: ",  frame_index, "/", total)
+                frame_index += 1
+                # x = input()
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):  # 按 'q' 退出
-                break
+                if cv2.waitKey(1) & 0xFF == ord('q'):  # 按 'q' 退出
+                    break
 
         else:
             break
 
-capture.release()
+cap.release()
